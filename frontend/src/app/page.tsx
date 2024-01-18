7ffae77fcb8a8dc5c61bee4e3aaa0458b5cb0e0f
Jean Carlo <jeancarlodev@gmail.com>
2024-01-18 18:00:15 -0300

    feat: init backend dev setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   caclulate_mmr,
   calculate_monthly_mmr,
@@ -5,157 +6,133 @@ import {
 } from '@/helpers/mmr';
 import Image from 'next/image';
 import { BarChart } from './components/chart';
+import React, { useEffect, useState } from 'react';
+import { stringify } from 'querystring';
 
 export default function Home() {
-  test_parse();
-  const mmr = caclulate_mmr();
-  const monthly_mmr = calculate_monthly_mmr();
+  const [mmr, setMmr] = useState<
+    [{ year: string; months: { [key: string]: number } }]
+  >([]);
 
-  console.log('mmr');
-  console.log(mmr);
+  const [selectedYears, setSelectedYears] = useState([]);
 
-  console.log('-----------');
-  console.log('mmr_mes');
-  console.log(monthly_mmr);
-
-  const months = [
-    '01',
-    '02',
-    '03',
-    '04',
-    '05',
-    '06',
-    '07',
-    '08',
-    '09',
-    '10',
-    '11',
-    '12',
+  const monthly_mmr = [
+    {
+      year: '2022',
+      months: {
+        '10': 6879.2125,
+        '11': 14806.711666666666,
+        '02': 28377.165833333336,
+        '01': 34545.439999999995,
+        '03': 17150.014166666668,
+        '06': 19798.00833333334,
+        '08': 12935.645833333334,
+        '09': 14661.144999999999,
+        '04': 21595.6775,
+        '07': 12296.3925,
+        '05': 15991.299166666666,
+      },
+    },
+    {
+      year: '2023',
+      months: {
+        '10': 3197.17,
+        '11': 8027.233333333331,
+        '12': 9385.48,
+        '01': 22753.017500000005,
+        '02': 4896.135,
+        '03': 17239.838333333333,
+        '06': 5492.438333333333,
+        '08': 6686.113333333334,
+        '09': 7191.044166666666,
+        '05': 15626.894166666665,
+        '07': 2689.3308333333334,
+        '04': 8149.5391666666665,
+      },
+    },
   ];
-  const mrr2022 = months.map((month) => monthly_mmr['2022'][month] || 0);
-  const mrr2023 = months.map((month) => monthly_mmr['2023'][month] || 0);
+
+  useEffect(() => {
+    setMmr(monthly_mmr);
+    const returned_years = monthly_mmr.map((item) => {
+      return item.year;
+    });
+
+    setSelectedYears(returned_years);
+  }, []);
+
+  const MonthNames = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ];
+
+  const months = MonthNames.map((name, index) => {
+    const monthNumber = (index + 1).toString().padStart(2, '0');
+    return { number: monthNumber, name: name };
+  });
+
+  const datasets = monthly_mmr
+    .filter((yearData) => selectedYears.includes(yearData.year))
+    .map((yearData) => {
+      const monthlyData = months.map(
+        (month) => yearData.months[month.number] || 0,
+      );
+      return {
+        label: yearData.year,
+        data: monthlyData,
+        backgroundColor: generateRandomColor(),
+      };
+    });
 
   const data = {
-    labels: months.map((month) => `Month ${month}`),
-    datasets: [
-      { label: '2022', data: mrr2022, backgroundColor: '#61DBFB' },
-      { label: '2023', data: mrr2023, backgroundColor: '#FF6384' },
-    ],
+    labels: months.map((month) => month.name),
+    datasets: datasets,
   };
+
+  function generateRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgba(${r}, ${g}, ${b}, 0.7)`;
+  }
+
+  function handleYearSelection(year: string) {
+    if (selectedYears.includes(year)) {
+      setSelectedYears(selectedYears.filter((y) => y !== year));
+    } else {
+      setSelectedYears([...selectedYears, year]);
+    }
+  }
+
   return (
     <main>
       <div>Hello world</div>
+
+      <div>
+        {['2022', '2023'].map((year) => (
+          <label key={year}>
+            <input
+              type="checkbox"
+              checked={selectedYears.includes(year)}
+              onChange={() => handleYearSelection(year)}
+            />
+            {year}
+          </label>
+        ))}
+      </div>
       <div>
         <BarChart data={data} />
       </div>
     </main>
-
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    //   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-    //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-    //       Get started by editing&nbsp;
-    //       <code className="font-mono font-bold">src/app/page.tsx</code>
-    //     </p>
-    //     <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-    //       <a
-    //         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         By{' '}
-    //         <Image
-    //           src="/vercel.svg"
-    //           alt="Vercel Logo"
-    //           className="dark:invert"
-    //           width={100}
-    //           height={24}
-    //           priority
-    //         />
-    //       </a>
-    //     </div>
-    //   </div>
-
-    //   <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-    //     <Image
-    //       className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js Logo"
-    //       width={180}
-    //       height={37}
-    //       priority
-    //     />
-    //   </div>
-
-    //   <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-    //     <a
-    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Docs{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Find in-depth information about Next.js features and API.
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Learn{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Learn about Next.js in an interactive course with&nbsp;quizzes!
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Templates{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Explore starter templates for Next.js.
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Deploy{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
-    //       </p>
-    //     </a>
-    //   </div>
-    // </main>
   );
 }
