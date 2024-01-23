e92c35703432cb07eebadc56a01c07692c1a91ef
Jean Carlo <jeancarlodev@gmail.com>
2024-01-23 10:09:43 -0300

    update deploy

---
+++ backend/src/dist/helpers/mrr/mrr.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculate_mrr = void 0;
+function calculate_mrr(array) {
+    let items_considered = 0;
+    const mrr_by_year_and_month = {};
+    const non_considered_items = [];
+    array.forEach((subscription) => {
+        // if (subscription.status === "Ativa") {
+        if (subscription.status && parseInt(subscription.quantidade_cobranca) !== 0) {
+            const date = new Date(subscription.data_inicio);
+            let month = date.getMonth();
+            let year = date.getFullYear();
+            let valor = parseFloat(subscription.valor.replace(",", "."));
+            let months_charged = parseInt(subscription.quantidade_cobranca);
+            let monthly_value = valor;
+            switch (parseInt(subscription.dias_cobranca)) {
+                case 365:
+                case 360:
+                    monthly_value /= 12;
+                    months_charged = 12;
+                    break;
+                case 30:
+                    break;
+                default:
+                    break;
+            }
+            for (let i = 0; i < months_charged; i++) {
+                if (!mrr_by_year_and_month[year]) {
+                    mrr_by_year_and_month[year] = {};
+                }
+                const month_key = String(month + 1).padStart(2, "0");
+                if (!mrr_by_year_and_month[year][month_key]) {
+                    mrr_by_year_and_month[year][month_key] = 0;
+                }
+                mrr_by_year_and_month[year][month_key] += monthly_value;
+                month++;
+                if (month > 11) {
+                    month = 0;
+                    year++;
+                }
+            }
+            items_considered++;
+            // }
+        }
+        else {
+            non_considered_items.push(subscription);
+        }
+    });
+    console.log("items considered for mrr: ", items_considered);
+    console.log(non_considered_items);
+    return Object.keys(mrr_by_year_and_month).map((year) => {
+        return { year: year, months: mrr_by_year_and_month[year] };
+    });
+}
+exports.calculate_mrr = calculate_mrr;
